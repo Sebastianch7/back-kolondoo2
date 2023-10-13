551a91ad504306271eede4555e5cd8c27ce11aa9
Sebastian Chaparro <sebastianchaparro@kolondoo.com>
2023-10-13 11:49:51 -0500

    servicios api fibra

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -10,11 +10,20 @@ use App\Models\Lead;
 
 class ApiController extends Controller
 {
+    /* consultas de las empresas que tienen servicios activos */
     public function getTarifasMovilList()
     {
         return DB::table('tarifasMovil')
             ->join('operadoras', 'operadoras.id', '=', 'tarifasMovil.operadora')
             ->select('tarifasMovil.*', 'operadoras.nombre', 'operadoras.logo')
+            ->get();
+    }
+    
+    public function getTarifasFibraList()
+    {
+        return DB::table('tarifasFibra')
+            ->join('operadoras', 'operadoras.id', '=', 'tarifasFibra.operadora')
+            ->select('tarifasFibra.*', 'operadoras.nombre', 'operadoras.logo')
             ->get();
     }
 
@@ -26,15 +35,16 @@ class ApiController extends Controller
             ->get();
     }
 
-    public function getOperadorasList()
+    public function getTarifasGasList()
     {
-        return DB::table('tarifasMovil')
-            ->join('operadoras', 'operadoras.id', '=', 'tarifasMovil.operadora')
-            ->select('operadoras.id', 'operadoras.nombre', 'operadoras.logo')
-            ->groupBy('operadora')
+        return DB::table('tarifasGas')
+            ->join('comercializadoras', 'comercializadoras.id', '=', 'tarifasGas.comercializadora')
+            ->select('tarifasGas.*', 'comercializadoras.nombre', 'comercializadoras.logo')
             ->get();
     }
+    /* fin consultas de las empresas que tienen servicios activos */
 
+    /* funciones para llamar las bases para los filtros de precio */
     public function getValuesFilterMovilList()
     {
         return DB::table('tarifasMovil')
@@ -42,12 +52,42 @@ class ApiController extends Controller
             ->get();
     }
 
+    public function getValuesFilterFibraList()
+    {
+        return DB::table('tarifasFibra')
+            ->selectRaw('ROUND(MAX(precio)+5) as max_precio, ROUND(MIN(precio)-5) as min_precio')
+            ->get();
+    }
+    /* fin funciones para filtros */
+
+    /* consultas por id de cada servicio */
     public function getDetailOfferMovilList($id)
     {
         return DB::table('tarifasMovil')
             ->join('operadoras', 'operadoras.id', '=', 'tarifasMovil.operadora')
             ->select('tarifasMovil.*', 'operadoras.nombre', 'operadoras.logo')
             ->where('tarifasMovil.id', '=', $id)
+            ->get();
+    }
+
+    public function getDetailOfferLuzList($id)
+    {
+        return DB::table('tarifasLuz')
+            ->join('comercializadoras', 'comercializadoras.id', '=', 'tarifasLuz.comercializadora')
+            ->select('tarifasLuz.*', 'comercializadoras.nombre', 'comercializadoras.logo')
+            ->where('tarifasLuz.id', '=', $id)
+            ->get();
+    }
+    /* fin funciones cosultas por id  */
+
+    /* funciones para llamar los 3 items adicionales para a page de thank */
+    public function getExtraOfferLuzList()
+    {
+        return DB::table('tarifasLuz')
+            ->join('comercializadoras', 'comercializadoras.id', '=', 'tarifasLuz.comercializadora')
+            ->select('tarifasLuz.*', 'comercializadoras.nombre', 'comercializadoras.logo')
+            ->inRandomOrder()
+            ->take(3)
             ->get();
     }
 
@@ -60,27 +100,9 @@ class ApiController extends Controller
             ->take(3)
             ->get();
     }
+    /* fin funciones para llamar 3 items adiconales */
 
-
-    public function getDetailOfferLuzList($id)
-    {
-        return DB::table('tarifasLuz')
-            ->join('comercializadoras', 'comercializadoras.id', '=', 'tarifasLuz.comercializadora')
-            ->select('tarifasLuz.*', 'comercializadoras.nombre', 'comercializadoras.logo')
-            ->where('tarifasLuz.id', '=', $id)
-            ->get();
-    }
-    
-    public function getExtraOfferLuzList()
-    {
-        return DB::table('tarifasLuz')
-            ->join('comercializadoras', 'comercializadoras.id', '=', 'tarifasLuz.comercializadora')
-            ->select('tarifasLuz.*', 'comercializadoras.nombre', 'comercializadoras.logo')
-            ->inRandomOrder()
-            ->take(3)
-            ->get();
-    }
-
+    /* funciones para consultar las ofertas comerciales */
     public function getComercializadorasList()
     {
         return DB::table('tarifasLuz')
@@ -89,6 +111,35 @@ class ApiController extends Controller
             ->groupBy('comercializadora')
             ->get();
     }
+
+    public function getComercializadorasGasList()
+    {
+        return DB::table('tarifasGas')
+            ->join('comercializadoras', 'comercializadoras.id', '=', 'tarifasGas.comercializadora')
+            ->select('comercializadoras.id', 'comercializadoras.nombre', 'comercializadoras.logo')
+            ->groupBy('comercializadora')
+            ->get();
+    }
+
+    public function getOperadorasList()
+    {
+        return DB::table('tarifasMovil')
+            ->join('operadoras', 'operadoras.id', '=', 'tarifasMovil.operadora')
+            ->select('operadoras.id', 'operadoras.nombre', 'operadoras.logo')
+            ->groupBy('operadora')
+            ->get();
+    }
+
+    public function getOperadorasFibraList()
+    {
+        return DB::table('tarifasFibra')
+            ->join('operadoras', 'operadoras.id', '=', 'tarifasFibra.operadora')
+            ->select('operadoras.id', 'operadoras.nombre', 'operadoras.logo')
+            ->groupBy('operadora')
+            ->get();
+    }
+    /* fin funciones para consultar ofertas comerciales */
+
 
 
     public function newLeadMobile(Request $request)
--- routes/api.php
+++ routes/api.php
@@ -4,14 +4,27 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ApiController;
 
-Route::get('getTarifasMovil', [ApiController::class, 'getTarifasMovilList']);
 Route::get('getOperadoras', [ApiController::class, 'getOperadorasList']);
+Route::get('getComercializadoras', [ApiController::class, 'getComercializadorasList']);
+Route::get('getComercializadorasGas', [ApiController::class, 'getComercializadorasGasList']);
+Route::get('getOperadorasFibra', [ApiController::class, 'getOperadorasFibraList']);
+/* movil */
+Route::get('getTarifasMovil', [ApiController::class, 'getTarifasMovilList']);
 Route::get('filterMovil', [ApiController::class, 'getValuesFilterMovilList']);
 Route::get('getDetailOfferMovil/{id}', [ApiController::class, 'getDetailOfferMovilList']);
 Route::get('getExtraOfferMovil', [ApiController::class, 'getExtraOfferMovilList']);
+/* Luz */
+Route::get('getTarifasLuz', [ApiController::class, 'getTarifasLuzList']);
 Route::get('getExtraOfferLuz', [ApiController::class, 'getExtraOfferLuzList']);
 Route::get('getDetailOfferLuz/{id}', [ApiController::class, 'getDetailOfferLuzList']);
+/* Gas */
+Route::get('getTarifasGas', [ApiController::class, 'getTarifasGasList']);
+Route::get('getExtraOfferGas', [ApiController::class, 'getExtraOfferGasList']);
+Route::get('getDetailOfferGas/{id}', [ApiController::class, 'getDetailOfferGasList']);
+/* Fibra */
+Route::get('getTarifasFibra', [ApiController::class, 'getTarifasFibraList']);
+Route::get('filterFibra', [ApiController::class, 'getValuesFilterFibraList']);
+Route::get('getExtraOfferFibra', [ApiController::class, 'getExtraOfferFibraList']);
+Route::get('getDetailOfferFibra/{id}', [ApiController::class, 'getDetailOfferFibraList']);
+/* Leads */
 Route::post('newLeadMobile', [ApiController::class, 'newLeadMobile']);
-//Tarifas Luz
-Route::get('getComercializadoras', [ApiController::class, 'getComercializadorasList']);
-Route::get('getTarifasLuz', [ApiController::class, 'getTarifasLuzList']);
